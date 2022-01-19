0b377f3d500e7bff2c66807dffad215ae6921dc3
Aaron Diable <aaron.diable@gmail.com>
2022-01-19 18:37:28 +0000

    remove password

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -3,7 +3,6 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'aaron',
-    password: 'k090674517',
     database: 'app1',
     max: 20,
     idleTimeoutMillis: 30000,
